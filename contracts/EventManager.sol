e03dd08b754f684155c859d45056a2da79c98e76
shegspear <shegspear@gmail.com>
2024-09-13 17:58:36 +0100

    a little update

--- contracts/EventManager.sol
+++ contracts/EventManager.sol
@@ -10,11 +10,12 @@ contract EventManager {
     error AddressZeroDetected();
     error NotEligible();
     error NotOwner();
+    error AlreadyCreatedEvent();
 
     event EventCompleted(address eventAddress);
     event EventLogged();
 
-    address myNFT = 0xa1200731B4Ae09e3DFeC1cEA7BFAEFB65F2e4E71;
+    address myNFT = 0xa1200731B4Ae09e3DFeC1cEA7BFAEFB65F2e4E71; // not tested 
 
     constructor() {
         owner = msg.sender;
@@ -32,8 +33,14 @@ contract EventManager {
 
     function createEvent(string memory _eventName) external  {
 
+        EventRequest storage _eventRequest = ledger[msg.sender];
+
         if(msg.sender == address(0)) {
             revert AddressZeroDetected();
+        }
+
+        if(_eventRequest.approved) {
+            revert AlreadyCreatedEvent();
         }
 
         if(IERC721(myNFT).balanceOf(msg.sender) ==  0) {
@@ -48,6 +55,11 @@ contract EventManager {
     // function acquire NFT  
 
     function updateEvent(address _event) external {
+
+        if(msg.sender == address(0)) {
+            revert AddressZeroDetected();
+        }
+
         if(msg.sender != owner) {
             revert NotOwner();
         }
